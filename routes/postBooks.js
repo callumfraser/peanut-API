5576578a7a6b1375dff9c21f8a54b926437e2e54
Dyllon Wade Isaacs <isaacsdyllon123@gmail.com>
2017-11-22 16:03:55 +0200

    changes made

--- routes/postBooks.js
+++ routes/postBooks.js
@@ -5,62 +5,62 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    let newBook = req.body;
-    let code = res.statusCode;
+  let newBook = req.body;
+  let code = res.statusCode;
 
-    bookModel.create({
-            recommendations: newBook.recommendations,
-            dateWritten: newBook.dateWritten,
-            bookName: newBook.bookName,
-            author: newBook.author,
-            genre: newBook.genre,
-            about: newBook.about
+  bookModel.create({
+      recommendations: newBook.recommendations,
+      dateWritten: newBook.dateWritten,
+      bookName: newBook.bookName,
+      author: newBook.author,
+      genre: newBook.genre,
+      about: newBook.about
+    })
+    .then((book) => {
+      res.json({
+        code,
+        book
+      })
+    })
+    .then(() => {
+      authorModel.findOne({
+          author: newBook.author
         })
-        .then((book) => {
-            res.json({
-                code,
-                book
-            })
+        .then((author) => {
+          if (!author) {
+            authorModel.create({
+                author: newBook.author,
+                books: newBook.bookName
+              })
+              .then((author) => {
+                res.json({
+                  code,
+                  author
+                });
+              })
+          } else {
+            authorModel.update({
+                author: newBook.author
+              }, {
+                $push: {
+                  books: newBook.bookName
+                }
+              })
+              .then((book) => {
+                res.json({
+                  code,
+                  book
+                });
+              })
+          }
         })
-        .then(() => {
-            authorModel.findOne({
-                    author: newBook.author
-                })
-                .then((author) => {
-                    if (!author) {
-                        authorModel.create({
-                            author: newBook.author,
-                            books: newBook.bookName
-                        })
-                        .then( (author) => {
-                            res.json({
-                                code,
-                                author
-                            });
-                        })
-                    } else {
-                        authorModel.update({
-                            author: newBook.author
-                        }, {
-                            $push: {
-                                books: newBook.bookName
-                            }
-                        })
-                        .then( (book) => {
-                            res.json({
-                                code,
-                                book
-                            });
-                        })
-                    }
-                })
-        })
-        .catch((err) => {
-            res.json({
-                code,
-                err
-            });
-        })
+    })
+    .catch((err) => {
+      res.json({
+        code,
+        err
+      });
+    })
 });
 
 module.exports = router;
