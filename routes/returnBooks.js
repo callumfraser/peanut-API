8f12a9b5388826aa76f0226d01ed9e54c0759ae6
Callum Fraser <callum@projectcodex.co>
2017-11-22 16:01:35 +0200

    fixed tracking of books read for users

--- routes/returnBooks.js
+++ routes/returnBooks.js
@@ -2,13 +2,16 @@
 const bookModel = require('../models/book.model');
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user.model');
 
 
-router.put('/return/:book_id/:recommended', (req, res) => {
+
+router.put('/return/:book_id/:user_id/:recommended', (req, res) => {
     let code = res.statusCode;
     var recommendedOrNot = req.params.recommended;
     var bookId = req.params.book_id;
     var recommendationCheck = 0;
+    var userId = req.params.user_id;
 
     if (recommendedOrNot == 'true'){
       recommendationCheck = 1;
@@ -40,6 +43,24 @@ router.put('/return/:book_id/:recommended', (req, res) => {
          status : "success",
          taken : result
        })
+       console.log(result.bookName);
+       userModel.findOneAndUpdate({
+         "_id": userId
+       }, {
+         $push: {
+             booksRead: result.bookName
+         }
+       }, function(err,userResult){
+         if (err){
+           console.log(userResult)
+           return res.json({
+             status : 'error',
+             error : err,
+             taken : []
+           })
+         }
+       })
+
      }
    })
 
