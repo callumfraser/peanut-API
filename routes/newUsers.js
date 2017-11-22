a8c4d4937198456532f685974846686352ce8b4a
Dyllon Wade Isaacs <isaacsdyllon123@gmail.com>
2017-11-22 14:19:06 +0200

    added some changes to newUsers

--- routes/newUsers.js
+++ routes/newUsers.js
@@ -4,6 +4,20 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
+  let code = res.statusCode;
+  userModel.find({})
+  .then((users) => {
+    res.json({
+      code,
+      users
+    });
+  })
+  .catch((err) => {
+    res.json({
+      code,
+      err
+    });
+  })
 
 });
 
