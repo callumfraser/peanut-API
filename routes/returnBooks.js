f46ec0395dd5634ff2f258756dd588dc193f6aeb
Callum Fraser <callum@projectcodex.co>
2017-11-22 15:14:51 +0200

    fixed route for return book

--- routes/returnBooks.js
+++ routes/returnBooks.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.put('return/:book_id/:recommended', (req, res) => {
+router.put('/return/:book_id/:recommended', (req, res) => {
     let code = res.statusCode;
     var recommendedOrNot = req.params.recommended;
     var bookId = req.params.book_id;
